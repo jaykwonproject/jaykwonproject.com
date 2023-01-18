e41ff451d0bfb47019c15e7792e66d52318866a9
Jay <jaykwonproject@gmail.com>
2023-01-18 16:49:31 -0500

    pdf changed to link version - testing2

--- client/src/resume.js
+++ client/src/resume.js
@@ -10,7 +10,7 @@ function resume() {
       {/*body section*/}      
       <div className="body">
         <h1>RESUME</h1>
-        <iframe src="https://www.w3docs.com/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="80%" height="100%"></iframe>
+        <iframe src="https://i.imgur.com/BBwRKV8.jpg" width="80%" height="850px"></iframe>
       </div>
 
 
