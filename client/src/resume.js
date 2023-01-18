a448564e5deb73a4aa33e2fa155d7d0c1458ba3a
Jay <jaykwonproject@gmail.com>
2023-01-18 16:28:57 -0500

    pdf change to iframe not object tag

--- client/src/resume.js
+++ client/src/resume.js
@@ -10,9 +10,7 @@ function resume() {
       {/*body section*/}      
       <div className="body">
         <h1>RESUME</h1>
-        <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
-          <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
-        </object>
+        <iframe src="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="100%" height="500px"></iframe>
       </div>
 
 
