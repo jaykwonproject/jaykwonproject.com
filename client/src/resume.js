59c28945f6cb7c96c1d724b4ac5db4239a1038ef
Jay <jaykwonproject@gmail.com>
2023-01-18 16:39:46 -0500

    pdf changed to link version - testing

--- client/src/resume.js
+++ client/src/resume.js
@@ -10,7 +10,7 @@ function resume() {
       {/*body section*/}      
       <div className="body">
         <h1>RESUME</h1>
-        <iframe src="HyukJoo_Resume.pdf" width="80%" height="100%"></iframe>
+        <iframe src="https://www.w3docs.com/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="80%" height="100%"></iframe>
       </div>
 
 
