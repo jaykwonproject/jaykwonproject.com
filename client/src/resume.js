520305d0972439fa478a30b75ac699f469ecb242
Jay <jaykwonproject@gmail.com>
2023-01-18 16:35:01 -0500

    fixd directory typo

--- client/src/resume.js
+++ client/src/resume.js
@@ -10,7 +10,7 @@ function resume() {
       {/*body section*/}      
       <div className="body">
         <h1>RESUME</h1>
-        <iframe src="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="100%" height="500px"></iframe>
+        <iframe src="HyukJoo_Resume.pdf" width="80%" height="100%"></iframe>
       </div>
 
 
