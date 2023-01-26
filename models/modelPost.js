94b1e2a6c5d349a331cd83785191b1ccb88b644d
Jay <jaykwonproject@gmail.com>
2023-01-26 08:48:03 -0500

    reverted the post model schema to original

--- models/modelPost.js
+++ models/modelPost.js
@@ -4,9 +4,8 @@ const postSchema = new mongoose.Schema({
     genre: {type:String},
     link: {type:String},
     title: {type:String},
-    description: {type:String},
-    timestmp: {type:String}
-}, {collection : 'test'})
+    description: {type:String}
+})
 
 const postModel = mongoose.model("post", postSchema);
 module.exports = postModel;
