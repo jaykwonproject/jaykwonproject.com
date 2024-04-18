5502e7e1945b67d547bcc026cb5060c6e374ffb0
jaykwonproject <jaykwonproject@gmail.com>
2024-04-17 21:51:27 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(cors({
 mongoose.connect(process.env.MONGODB_URI);
 
 const port = process.env.PORT || 3001;
+app.options('*', cors());
 
 app.get("/getPosts",(req,res)=>{
     postModel.find().then(studyCollection=>res.json(studyCollection));
