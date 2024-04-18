e8a58e2a9ac8d287e413ebd287d44c2fb6503ac1
jaykwonproject <jaykwonproject@gmail.com>
2024-04-18 15:03:11 -0400

    Update study.js

--- client/src/study.js
+++ client/src/study.js
@@ -19,7 +19,7 @@ function Study() {
   const[postOrder, setPostOrder] = useState("");
   
   useEffect(()=>{
-    fetch('https://web-production-be99.up.railway.app/getPosts',{
+    fetch('https://jaykwonproject-com-backend.vercel.app/getPosts',{
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
